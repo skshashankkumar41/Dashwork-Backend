0bf7ed6c29c2c5fbf5743740783660a9f2080303
Shashank Srivastava <skshashankkumar41@gmail.com>
2021-04-28 23:44:41 +0530

    created seperate module for database conection

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import json 
-import pymongo
 from bson import json_util
 from flask import Flask,jsonify
 from flask_cors import CORS, cross_origin
 from flask_restful import Api, Resource
 from resources.add_intent import AddIntent
 from resources.get_intents import GetIntent
+from mongo_connector import connector
 
 app = Flask(__name__)
 api = Api(app)
@@ -16,10 +16,7 @@ cors = CORS(app, resources={
     }
 })
 
-
-client = pymongo.MongoClient('mongodb://127.0.0.1:27017/')
-db = client['dashwork']
-intent_collection = db.intent
+intent_collection = connector()
 
 api.add_resource(AddIntent, "/add_intent/" ,resource_class_kwargs={'collection': intent_collection})
 api.add_resource(GetIntent, "/get_intents/" ,resource_class_kwargs={'collection': intent_collection})
---
+++ mongo_connector.py
@@ -0,0 +1,7 @@
+import pymongo
+
+def connector():
+    client = pymongo.MongoClient('mongodb://127.0.0.1:27017/')
+    db = client['dashwork']
+    intent_collection = db.intent
+    return intent_collection
